d09cd8126cf45fc4fb9f8b217fe6de740c8c91f5
seisvelas <44826516+seisvelas@users.noreply.github.com>
2019-03-06 13:55:56 -0600

    Update Book.sql
    formatted instructions

--- Book.sql
+++ Book.sql
@@ -1,12 +1,14 @@
 /*
-You work at a book store. It's the end of the month, and you need to find out the 5 bestselling books at your store. Use a select statement to list names, authors, and number of copies sold of the 5 books which were sold most.
+You work at a book store. It's the end of the month, and you 
+need to find out the 5 bestselling books at your store. Use a 
+select statement to list names, authors, and number of copies sold 
+of the 5 books which were sold most.
 
 books table schema
-
+------------------
 name
 author
 copies_sold
-NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
 */
 
 SELECT name, author, copies_sold 
