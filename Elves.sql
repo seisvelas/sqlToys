681b122a9f4661da0099741886990292425c6ab6
seisvelas <44826516+seisvelas@users.noreply.github.com>
2019-03-06 13:54:13 -0600

    Update Elves.sql

--- Elves.sql
+++ Elves.sql
@@ -1,3 +1,5 @@
+-- In a single shortlist column, return each elf's full name (capitalized first letters) if they have tegil in the first name or astar in the last name
+
 SELECT Concat(Concat(Initcap(firstname), ' '), Initcap(lastname)) AS shortlist 
 FROM   elves 
 WHERE  firstname LIKE '%tegil%' 
