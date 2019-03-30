347889be542c66f7f6ba173cc14b5615c0fd78d6
seisvelas <44826516+seisvelas@users.noreply.github.com>
2019-03-30 13:04:12 -0600

    Update UnionAll.sql
    forgot to do only price>50

--- UnionAll.sql
+++ UnionAll.sql
@@ -1,9 +1,10 @@
-SELECT 'US' as location, *
+SELECT t.* FROM 
+(SELECT 'US' as location, *
 FROM ussales
 UNION ALL
 SELECT 'EU' as location, *
-FROM eusales;
-
+FROM eusales) t
+WHERE t.price > 50;
 /*
 
 For this challenge you need to create a UNION statement, there are two tables ussales and eusales the parent company tracks each sale at its respective location in each table, you must all filter the sale price so it only returns rows with a sale greater than 50.00. You have been tasked with combining that data for future analysis. Order by US then EU sales.
