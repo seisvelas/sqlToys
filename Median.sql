520d11955ba497a0d791afe463b5e6f8279db8a2
seisvelas <44826516+seisvelas@users.noreply.github.com>
2019-03-30 11:58:59 -0600

    Update Median.sql
    Forgot to add instructions

--- Median.sql
+++ Median.sql
@@ -9,3 +9,16 @@ SELECT
          WHERE t.rc=t.total_rows::float / 2 + 1)::float as median,
        MAX(r.score)
 FROM result r
+
+/*
+
+For this challenge you need to create a simple SELECT statement. Your task is to calculate the MIN, MEDIAN and MAX scores of the students from the results table.
+
+Tables and relationship below:
+
+Resultant table:
+min
+median
+max
+
+*/
