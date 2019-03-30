9081b99fcb807108771dcc6fe5523e1fb8ab1a17
seisvelas <44826516+seisvelas@users.noreply.github.com>
2019-03-30 13:01:12 -0600

    Create UnionAll.sql

---
+++ UnionAll.sql
@@ -0,0 +1,27 @@
+SELECT 'US' as location, *
+FROM ussales
+UNION ALL
+SELECT 'EU' as location, *
+FROM eusales;
+
+/*
+
+For this challenge you need to create a UNION statement, there are two tables ussales and eusales the parent company tracks each sale at its respective location in each table, you must all filter the sale price so it only returns rows with a sale greater than 50.00. You have been tasked with combining that data for future analysis. Order by US then EU sales.
+
+(us/eu)sales table schema
+id
+name
+price
+card_name
+card_number
+transaction_date
+resultant table schema
+location (EU for eusales and US for ussales)
+id
+name
+price (greater than 50.00)
+card_name
+card_number
+transaction_date
+
+*/
